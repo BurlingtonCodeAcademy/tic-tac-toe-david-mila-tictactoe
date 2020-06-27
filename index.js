31758a95cd4dd5280568f35e2877b45afbfbbbd3
Mila1444 <66134690+Mila1444@users.noreply.github.com>
2020-06-27 01:12:38 -0400

    added names

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let board = document.getElementById('board')
 let gameStatus = document.getElementById('game-status')
 let startButton = document.getElementById('start')
 let cells = document.getElementsByClassName('cell')
-let PlayerOneName = document.getElementById('PlayerOneName')
-let PlayerTwoName = document.getElementById('PlayerTwoName')
+let playerOneName = document.getElementById('playerOneName')
+let playerTwoName = document.getElementById('playerTwoName')
 let clock = document.getElementById('clock')
 
 
@@ -22,6 +22,8 @@ let playerOneArr = []
 let playerTwoArr = []
 let winCom = []
 let clockCount = 0
+let playerOneDisplay = ""
+let playerTwoDisplay = ""
 //start function - when start button gets clicked
 function start() {
     currentPlayer = 1
@@ -40,22 +42,22 @@ function start() {
     playerOneArr = []
     playerTwoArr = []
     winCom = []
-    //clock
-    interval = setInterval(() => {updateClock()}, 1000)
+    //setting up clock
+    interval = setInterval(() => { updateClock() }, 1000)
     clockCount = 0
     clock.innerHTML = `Time Elapse: 0 seconds`
     //two players
-    PlayerOneName  = (PlayerOneName.value === "" ? "X" : PlayerOneName.value)
-    PlayerTwoName  = (PlayerTwoName.value === "" ? "X" : PlayerTwoName.value)
-    // PlayerOneName.disabled = true 
-    // PlayerTwoName.disabled = true
+    playerOneDisplay = (playerOneName.value === "" ? "X" : playerOneName.value)
+    playerTwoDisplay = (playerTwoName.value === "" ? "O" : playerTwoName.value)
+    playerOneName.disabled = true
+    playerTwoName.disabled = true
 }
 
 //adding event listener to start button
 startButton.addEventListener('click', start);
 
 //function to update clock
-function updateClock () {
+function updateClock() {
     clockCount++
     clock.innerHTML = `Time Elapse: ${clockCount} seconds`
 }
@@ -63,9 +65,9 @@ function updateClock () {
 //function to change status
 function showPlayer() {
     if (currentPlayer === 1) {
-        gameStatus.innerHTML = `Player ${playerOne}'s turn`
+        gameStatus.innerHTML = `Player ${playerOneDisplay}'s turn`
     } else {
-        gameStatus.innerHTML = `Player ${playerTwo}'s turn`
+        gameStatus.innerHTML = `Player ${playerTwoDisplay}'s turn`
     }
 }
 
@@ -73,7 +75,6 @@ function showPlayer() {
 function alreadyClicked() {
     gameStatus.innerHTML = `Please select an empty cell.`
 }
-
 
 //function to check for wins
 function checkWin(currentPlayerArr) {
@@ -101,6 +102,10 @@ function reStart() {
         cell.removeEventListener('click', alreadyClicked)
     }
     startButton.disabled = false
+    playerOneName.disabled = false
+    playerTwoName.disabled = false
+    playerOneName.value = ""
+    playerTwoName.value = ""
 }
 
 //makeMove function - when player clicks on cells
@@ -121,8 +126,9 @@ function clicked(event) {
         event.target.style.color = "red"
         currentPlayer += 1
         playerOneArr.push(cellNumber)
+
         if (checkWin(playerOneArr) === true) {
-            gameStatus.innerHTML = `Congratulations ${playerOne}! You won!`
+            gameStatus.innerHTML = `Congratulations ${playerOneDisplay}! You won!`
             clearInterval(interval)
 
             for (let cell of cells) {
@@ -139,7 +145,7 @@ function clicked(event) {
         currentPlayer -= 1
         playerTwoArr.push(cellNumber)
         if (checkWin(playerTwoArr) === true) {
-            gameStatus.innerHTML = `Congratulations ${playerTwo}! You won!`
+            gameStatus.innerHTML = `Congratulations ${playerTwoDisplay}! You won!`
             clearInterval(interval)
 
             for (let cell of cells) {
@@ -151,8 +157,14 @@ function clicked(event) {
             return reStart()
         }
     }
+
     showPlayer()
 }
+
+// if (boardArr = []) {
+//     gameStatus.innerHTML = `It's a draw`
+//     clearInterval(interval)
+//     startButton.disabled = false
 
 
 
